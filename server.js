cbeffcb7dadf615c6e92f8dcbe969b543683a806
Brian Lynch <brianlynch@Brians-MacBook-Pro.local>
2018-08-03 17:18:25 -0400

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
-friendsArray = require("./App/Data/friends.js");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
